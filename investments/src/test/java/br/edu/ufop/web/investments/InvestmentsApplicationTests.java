a68ef4149c5cb1f7d167167748ca19b6cb392916
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2026-02-21 12:34:22 -0300

    estrutura inicial do projeto

---
+++ investments/src/test/java/br/edu/ufop/web/investments/InvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufop.web.investments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
